eababe3f3b13c72fa58e77c92bc17657b2759399
h.js <121519834+hyeon-js@users.noreply.github.com>
2023-12-30 06:35:37 +0900

    init

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# Traffic Card Balance Checker Android App in Korea and Japan
+# 한국 & 일본 교통카드 잔액 확인 앱
 © 2023 Hyeon.js, All rights reserved.
-
-- 한국 & 일본 교통카드 잔액 확인 앱
 
 ## 지원하는 카드
 ### 한국
---
+++ java/com/hyeonjs/trafficcardreader/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.hyeonjs.trafficcardreader
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val layout = LinearLayout(this)
+        layout.orientation = 1
+
+
+        setContentView(layout)
+    }
+}
