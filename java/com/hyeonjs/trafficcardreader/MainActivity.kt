388fc2905a9a3d7b719ff484b9d95ad770058fb1
h.js <121519834+hyeon-js@users.noreply.github.com>
2024-01-07 04:14:22 +0900

    Update MainActivity.kt

--- java/com/hyeonjs/trafficcardreader/MainActivity.kt
+++ java/com/hyeonjs/trafficcardreader/MainActivity.kt
@@ -2,7 +2,9 @@ package com.hyeonjs.trafficcardreader
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,11 @@ class MainActivity : Activity() {
         val layout = LinearLayout(this)
         layout.orientation = 1
 
+        val txt = TextView(this)
+        txt.text = "Tag IC Card to Smartphone"
+        txt.layoutParams = LinearLayout.LayoutParams(-1, -1)
+        txt.gravity = Gravity.CENTER or Gravity.CENTER_VERTICAL
+        layout.addView(txt)
 
         setContentView(layout)
     }
